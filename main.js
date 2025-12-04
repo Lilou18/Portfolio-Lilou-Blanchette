27e2ac742312c3272c701945120b5a46e61c3cac
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2025-12-04 01:07:32 -0500

    again

--- main.js
+++ main.js
@@ -28,16 +28,16 @@ k.scene("level", async () => {
         console.log("Mobile controls displayed");
     }
 
-    // console.log("Loading level data...");
-    // // Load level data
-    // const levelData = await fetch("./map/level2.json");
-    // const levelDataJson = await levelData.json();
-    // console.log("Level data loaded:", levelDataJson);
+    console.log("Loading level data...");
+    // Load level data
+    const levelData = await fetch("./map/level2.json");
+    const levelDataJson = await levelData.json();
+    console.log("Level data loaded:", levelDataJson);
 
-    // console.log("Initializing level...");
-    // // Initialize the level
-    // level(k, levelDataJson);
-    // console.log("Level initialized");
+    console.log("Initializing level...");
+    // Initialize the level
+    level(k, levelDataJson);
+    console.log("Level initialized");
 
     // // Create the player
     // let playerPosition = levelDataJson.layers[6].objects[0];
