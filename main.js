ab1598990c42884251d6e7071a6ca96f87aa2fe3
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2026-01-19 16:07:49 -0500

    remove player

--- main.js
+++ main.js
@@ -466,10 +466,10 @@ k.scene("level", async () => {
         const levelControl = level(k, levelDataJson);
 
         // Create the player
-        let playerPosition = levelDataJson.layers[6].objects[0];
-        const player = new Player(k, playerPosition.x, playerPosition.y, 400, 670);
-        gameState.player = player;
-        levelControl.setPlayer(gameState.player);
+        //let playerPosition = levelDataJson.layers[6].objects[0];
+        //const player = new Player(k, playerPosition.x, playerPosition.y, 400, 670);
+        //gameState.player = player;
+        //levelControl.setPlayer(gameState.player);
 
         // Setup the camera
         //const camera = new Camera(player.gameObject, levelControl);
