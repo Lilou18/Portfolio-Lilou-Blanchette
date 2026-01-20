4df4c27c15438747faad632ea1774581d2dfe584
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2026-01-20 18:17:34 -0500

    a lot of gameObject

--- main.js
+++ main.js
@@ -428,41 +428,143 @@
 
 
 
-import { k } from "./loader.js";
-import { level } from "./level.js";
-//import { gameState } from "./gameState.js";
-//import { Player } from "./player.js";
-//import { Camera } from "./camera.js";
-//import { uiManager } from "./uiManager.js";
-//import { applicationManager } from "./applicationManager.js";
-import { createWorld } from "./animationManager.js";
+// import { k } from "./loader.js";
+// import { level } from "./level.js";
+// //import { gameState } from "./gameState.js";
+// //import { Player } from "./player.js";
+// //import { Camera } from "./camera.js";
+// //import { uiManager } from "./uiManager.js";
+// //import { applicationManager } from "./applicationManager.js";
+// import { createWorld } from "./animationManager.js";
 
-k.scene("level", async () => {
-    // Load level data
-    const levelData = await fetch("level2.json");
-    const levelDataJson = await levelData.json();
+// k.scene("level", async () => {
+//     // Load level data
+//     const levelData = await fetch("level2.json");
+//     const levelDataJson = await levelData.json();
 
-    k.onLoad(() => {
-        const world = createWorld();
+//     k.onLoad(() => {
+//         const world = createWorld();
 
-        // Initialize the level
-        const levelControl = level(k, levelDataJson, world);
+//         // Initialize the level
+//         const levelControl = level(k, levelDataJson, world);
 
-        // // Create the player
-        // let playerPosition = levelDataJson.layers[6].objects[0];
-        // const player = new Player(k, playerPosition.x, playerPosition.y, 400, 670);
-        // gameState.player = player;
-        // levelControl.setPlayer(gameState.player);
+//         // // Create the player
+//         // let playerPosition = levelDataJson.layers[6].objects[0];
+//         // const player = new Player(k, playerPosition.x, playerPosition.y, 400, 670);
+//         // gameState.player = player;
+//         // levelControl.setPlayer(gameState.player);
 
-        // Setup the camera
-        //const camera = new Camera(player.gameObject, levelControl);
-        //camera.setMapParts(levelControl.getMapParts());
+//         // Setup the camera
+//         //const camera = new Camera(player.gameObject, levelControl);
+//         //camera.setMapParts(levelControl.getMapParts());
 
-        //uiManager.setUpHologramInteractions();
+//         //uiManager.setUpHologramInteractions();
 
-        //uiManager.initializeMobileControls();
+//         //uiManager.initializeMobileControls();
 
-    });
+//     });
+
+// });
+
+// k.go("level");
+
+
+
+
+import kaplay from "https://unpkg.com/kaplay@4000.0.0-alpha.23/dist/kaplay.mjs";
+
+export const k = kaplay({
+    canvas: document.getElementById("gameCanvas"),
+    background: "#5ba675",
+    texFilter: "linear",
+    crisp: false,
+    pixelDensity: 1,
+    touchToMouse: true,
+    pixelDensity: 1,
+    // pixelDensity: Math.min(2, window.devicePixelRatio),
+});
+
+
+
+k.scene("level", () => {
+
+    k.loadSprite("levelP1", "./assets/levelP1.png");
+    k.loadSprite("levelP2", "./assets/levelP2.png");
+    k.loadSprite("levelP3", "./assets/levelP3.png");
+
+    const mapPart1 = k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart2 = k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart3 = k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart4 = k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart5 = k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart6 = k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart7 = k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart8 = k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart9 = k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart10 = k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart11 = k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart12 = k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart13 = k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart14= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart15 = k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart16 = k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart17= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart18= k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart19= k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart20= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart21= k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart22= k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart23= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart24= k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart25= k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart26= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart27= k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart28= k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart29 = k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart30 = k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart31 = k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart32 = k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart33 = k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart34= k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart35= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart36= k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart37= k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart38= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart39= k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart40= k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart41= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart42= k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart43= k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart44= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart45= k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart46= k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart47= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart48= k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart49= k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart50= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart51= k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+    const mapPart52= k.add([pos(0, 0), sprite("levelP1"), k.z(0),]);
+    const mapPart53= k.add([pos(0, 0), sprite("levelP2"), k.z(0),]);
+    const mapPart54= k.add([pos(0, 0), sprite("levelP3"), k.z(0),]);
+
+
 
 });
 
